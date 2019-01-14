bbaceb6b150cd11d349b3623256d63321899e26b
James Waren <jamesdonew@gmail.com>
2019-01-14 13:33:25 -0500

    fixing scrolling

--- components/Phoneinput.js
+++ components/Phoneinput.js
@@ -4,6 +4,8 @@ import PhoneInput from 'react-phone-input-auto-format';
 export default class Phoneinput extends React.Component {
   constructor(props) {
     super(props);
+    this.inputField = React.createRef();
+    this.scrollOnFocus = this.scrollOnFocus.bind(this);
   }
 
   handleChange = value => {
@@ -14,9 +16,19 @@ export default class Phoneinput extends React.Component {
     this.props.onBlurEvent(this.props.fieldname, true);
   };
 
+  scrollOnFocus() {
+    setTimeout(() => {
+      window.scrollTo(0, this.inputField.current.getBoundingClientRect().top);
+    }, 300);
+  }
+
   render() {
     return (
-      <div className="input__wrapper">
+      <div
+        ref={this.inputField}
+        onClick={this.scrollOnFocus}
+        className="input__wrapper"
+      >
         <PhoneInput
           onChange={this.handleChange}
           onBlur={this.handleBlur}
