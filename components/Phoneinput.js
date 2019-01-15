c58e37521cd42c38ae0deee58e27fa3309a6685c
smartdev328 <solid.dev3028@gmail.com>
2019-01-15 12:53:36 -0500

    fixing iphone jump from email input to phone

--- components/Phoneinput.js
+++ components/Phoneinput.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import PhoneInput from 'react-phone-input-auto-format';
-import { Field } from 'formik';
+import NumberFormat from 'react-number-format';
 
 export default class Phoneinput extends React.Component {
   constructor(props) {
     super(props);
   }
 
-  handleChange = value => {
-    this.props.onChangeEvent(this.props.fieldname, value);
+  handleChange = e => {
+    this.props.onChangeEvent(this.props.fieldname, e.target.value);
+  };
+
+  handleBlur = () => {
+    this.props.onBlurEvent(this.props.fieldname, true);
   };
 
   render() {
     return (
       <div className="input__wrapper">
-        <PhoneInput
+        <NumberFormat
           onChange={this.handleChange}
+          onBlur={this.handleBlur}
+          format="(###) ###-####"
           name={this.props.fieldname}
           id={this.props.fieldname}
         />
