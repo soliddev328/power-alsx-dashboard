c1248662c47fa353a6bf3bec14a3db001aeb5c6d
Ardavan Metghalchi <ardavan.metghalchi@gmail.com>
2019-01-16 15:10:15 -0500

    saving address and utility when saving the lead

--- pages/onboarding/step5.js
+++ pages/onboarding/step5.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Router from 'next/router';
 import { Formik, Form } from 'formik';
 import axios from 'axios';
+import Phoneinput from '../../components/Phoneinput';
 import Header from '../../components/Header';
 import Input from '../../components/Input';
-import Phoneinput from '../../components/Phoneinput';
 import SingleStep from '../../components/SingleStep';
 import Button from '../../components/Button';
 import CONSTANTS from '../../globals';
@@ -19,13 +19,31 @@ class Step5 extends React.Component {
 
   componentDidMount() {
     let storedName = '';
+    let storedUtility = '';
+    let storedAgreementChecked = false;
+    let storedAddress = '';
+
+    if (localStorage.getItem('utility')) {
+      storedUtility = JSON.parse(localStorage.getItem('utility'));
+    }
+    if (localStorage.getItem('acceptedTermsAndConditions')) {
+      storedAgreementChecked = JSON.parse(
+        localStorage.getItem('acceptedTermsAndConditions')
+      );
+    }
+    if (localStorage.getItem('address')) {
+      storedAddress = JSON.parse(localStorage.getItem('address'));
+    }
 
     if (localStorage.getItem('username')) {
       storedName = JSON.parse(localStorage.getItem('username'));
     }
 
     this.setState({
-      name: storedName
+      name: storedName,
+      utility: storedUtility.label,
+      address: storedAddress,
+      agreedTermsAndConditions: storedAgreementChecked
     });
   }
 
@@ -50,7 +68,13 @@ class Step5 extends React.Component {
                   FirstName: this.state.name.firstName,
                   LastName: this.state.name.lastName,
                   Phone: values.phoneNumber,
-                  Email: values.emailAddress
+                  Email: values.emailAddress,
+                  street: this.state.address.street,
+                  state: this.state.address.state,
+                  city: this.state.address.city,
+                  postalCode: this.state.address.postalCode,
+                  agreementChecked: !!this.state.agreedTermsAndConditions,
+                  utility: this.state.utility
                 })
                 .then(function(response) {
                   localStorage.setItem('leadId', response.data.data.leadId);
