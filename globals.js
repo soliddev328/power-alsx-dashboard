608edc05f091f52c1c6cfa216d2e98592720936d
Ardavan Metghalchi <ardavan.metghalchi@gmail.com>
2019-01-17 15:08:11 -0500

    adding fullstory key as a var

--- globals.js
+++ globals.js
@@ -3,6 +3,7 @@ module.exports = {
   dev: {
     API: "https://comenergy-api-staging.herokuapp.com",
     SEGMENT_KEY: "zAFMGeybdkriupfPqCYanAHc9HUNhAug",
+    FULLSTORY_KEY: "NULL",
     STRIPE_KEY: "pk_test_XZitL2jhXqRuAGQuzzYnIcwW",
     PLAID_KEY: "6a39ce056577235f452bcf275b5191",
     FB_APP_ID: "354393852058288"
@@ -10,6 +11,7 @@ module.exports = {
   prod: {
     API: "https://api.commonenergy.us",
     SEGMENT_KEY: "5zpSiwIkrWVXtGA2t2Xvuv42XQYYErAB",
+    FULLSTORY_KEY: "7ZZDY",
     STRIPE_KEY: "pk_live_MxPO1LbCVcS8l7SnDOiGfBmQ",
     PLAID_KEY: "6a39ce056577235f452bcf275b5191",
     FB_APP_ID: "1861844694112540"
