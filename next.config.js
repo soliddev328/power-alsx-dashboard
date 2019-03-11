780751bf43ec43ef1d297f9c5a5ffa5a66cd9bbb
James Waren <jamesdonew@gmail.com>
2019-03-11 16:45:17 -0400

    fixing zeit error: No serverless pages were built

--- next.config.js
+++ next.config.js
@@ -1,17 +1,18 @@
 // next.config.js
-const fs = require('fs');
-const { join } = require('path');
-const { promisify } = require('util');
+const fs = require("fs");
+const { join } = require("path");
+const { promisify } = require("util");
 const copyFile = promisify(fs.copyFile);
 
 module.exports = {
+  target: "serverless",
   exportPathMap: async function(defaultPathMap, { dev, dir, outDir }) {
     if (dev) {
       return defaultPathMap;
     }
     // This will copy robots.txt from your project root into the out directory
-    await copyFile(join(dir, 'robots.txt'), join(outDir, 'robots.txt'));
-    await copyFile(join(dir, 'sitemap.xml'), join(outDir, 'sitemap.xml'));
+    await copyFile(join(dir, "robots.txt"), join(outDir, "robots.txt"));
+    await copyFile(join(dir, "sitemap.xml"), join(outDir, "sitemap.xml"));
     return defaultPathMap;
   }
 };
