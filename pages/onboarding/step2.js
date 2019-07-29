60534cc263334e0ab91ca9851697354474e87362
Robert Zhang <solid.dev3028@gmail.com>
2019-07-29 13:56:33 -0300

    apply feedback

--- pages/onboarding/step2.js
+++ pages/onboarding/step2.js
@@ -4,7 +4,6 @@ import { Formik, Form } from "formik"
 import axios from "axios"
 import GeoSuggest from "../../components/GeoSuggest"
 import Header from "../../components/Header"
-import Input from "../../components/Input"
 import SingleStep from "../../components/SingleStep"
 import Button from "../../components/Button"
 import Stepper from "../../components/Stepper"
@@ -36,18 +35,10 @@ class Step2 extends React.Component {
     if (localStorage.getItem("leadId")) {
       storedLeadId = localStorage.getItem("leadId")
     }
-    if (localStorage.getItem("fname")) {
-      storedFname = localStorage.getItem("fname")
-    }
-    if (localStorage.getItem("lname")) {
-      storedLname = localStorage.getItem("lname")
-    }
 
     this.setState({
       postalCode: storedPostalCode,
-      leadId: storedLeadId,
-      firstName: storedFname,
-      lastName: storedLname
+      leadId: storedLeadId
     })
   }
 
@@ -81,8 +72,6 @@ class Step2 extends React.Component {
         <SingleStep title="And what is your name and address please?">
           <Formik
             initialValues={{
-              firstName: this.state.firstName,
-              lastName: this.state.lastName,
               address: ""
             }}
             onSubmit={values => {
@@ -100,13 +89,7 @@ class Step2 extends React.Component {
                 apt: values.apt ? values.apt : ""
               }
 
-              const name = {
-                firstName: values.firstName,
-                lastName: values.lastName
-              }
-
               localStorage.setItem("address", JSON.stringify(address))
-              localStorage.setItem("username", JSON.stringify(name))
 
               if (address.postalCode === this.state.postalCode) {
                 window.firebase
@@ -118,8 +101,6 @@ class Step2 extends React.Component {
                         `${API}/v1/subscribers`,
                         {
                           leadId: this.state.leadId,
-                          firstName: values.firstName,
-                          lastName: values.lastName,
                           street: address.street,
                           state: address.state,
                           city: address.city
@@ -131,10 +112,6 @@ class Step2 extends React.Component {
                         }
                       )
                       .then(() => {
-                        global.analytics.identify(this.state.leadId, {
-                          firstName: values.firstName,
-                          lastName: values.lastName
-                        })
                         Router.push({
                           pathname: "/onboarding/step3"
                         })
@@ -150,10 +127,6 @@ class Step2 extends React.Component {
             }}
             render={props => (
               <Form>
-                <div className="two-columns two-columns--responsive">
-                  <Input label="First Name" fieldname="firstName" autoFocus />
-                  <Input label="Last Name" fieldname="lastName" />
-                </div>
                 <GeoSuggest
                   label="Address"
                   fieldname="address"
@@ -164,14 +137,7 @@ class Step2 extends React.Component {
                   touched={props.touched.topics}
                 />
                 <p className="error">{this.state.errorMessage}</p>
-                <Button
-                  primary
-                  disabled={
-                    !props.values.firstName != "" ||
-                    !props.values.lastName != "" ||
-                    !props.values.address != ""
-                  }
-                >
+                <Button primary disabled={!props.values.address != ""}>
                   Next
                 </Button>
               </Form>
