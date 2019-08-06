d061d61af2bdd3eafadab240100d4853cabf609e
Robert Zhang <solid.dev3028@gmail.com>
2019-08-06 00:14:53 -0300

    started working on new flow modifications

--- pages/onboarding/step3.js
+++ pages/onboarding/step3.js
@@ -2,7 +2,7 @@ import React from "react"
 import Router from "next/router"
 import { Formik, Form } from "formik"
 import axios from "axios"
-import Phoneinput from "../../components/Phoneinput"
+import GeoSuggest from "../../components/GeoSuggest"
 import Header from "../../components/Header"
 import SingleStep from "../../components/SingleStep"
 import Button from "../../components/Button"
@@ -15,117 +15,138 @@ const { API } =
 class Step3 extends React.Component {
   constructor(props) {
     super(props)
+
     this.state = {
-      name: ""
+      postalCode: "",
+      errorMessage: ""
     }
   }
 
   componentDidMount() {
     global.analytics.page("Step 3")
-
+    let storedPostalCode = ""
     let storedLeadId = ""
-    let storedName = ""
-    let storedUtilityPaperOnly = false
+    let storedFname = ""
+    let storedLname = ""
 
+    if (localStorage.getItem("postalCode")) {
+      storedPostalCode = JSON.parse(localStorage.getItem("postalCode"))
+    }
     if (localStorage.getItem("leadId")) {
       storedLeadId = localStorage.getItem("leadId")
     }
-    if (localStorage.getItem("username")) {
-      storedName = JSON.parse(localStorage.getItem("username"))
-    }
-
-    if (localStorage.getItem("utility")) {
-      let storedUtility = JSON.parse(localStorage.getItem("utility"))
-      if (storedUtility && storedUtility.paperOnly)
-        storedUtilityPaperOnly = true
-    }
 
     this.setState({
-      leadId: storedLeadId,
-      name: storedName,
-      storedUtilityPaperOnly
+      postalCode: storedPostalCode,
+      leadId: storedLeadId
     })
   }
 
-  capitalize(word) {
-    return word && word[0].toUpperCase() + word.slice(1)
+  getPostalCode(values) {
+    const components = values.address
+      ? values.address.gmaps.address_components
+      : null
+
+    const postalCode = components
+      ? components.find(x => x.types[0] == "postal_code")
+      : null
+
+    return postalCode ? postalCode.long_name : ""
+  }
+
+  getStateAddress(values) {
+    const components = values.address
+      ? values.address.gmaps.address_components
+      : null
+    const state = components
+      ? components.find(x => x.types[0] == "administrative_area_level_1")
+      : null
+
+    return state ? state.short_name : ""
   }
 
   render() {
     return (
       <main>
         <Header />
-        <SingleStep
-          title={`Welcome ${this.capitalize(
-            this.state.name.firstName
-          )}! What phone number would you like to use with the account?`}
-        >
+        <SingleStep title="And what is your address please?">
           <Formik
             initialValues={{
-              phoneNumber: ""
+              address: ""
             }}
             onSubmit={values => {
-              localStorage.setItem("phoneNumer", values.phoneNumber)
-              window.firebase
-                .auth()
-                .currentUser.getIdToken(true)
-                .then(idToken => {
-                  axios
-                    .put(
-                      `${API}/v1/subscribers`,
-                      {
-                        leadId: this.state.leadId,
-                        phone: values.phoneNumber
-                      },
-                      {
-                        headers: {
-                          Authorization: idToken
+              const arrayAddress = values.address.description.split(",")
+              const street = arrayAddress[0] ? arrayAddress[0] : ""
+              const city = arrayAddress[1]
+                ? arrayAddress[1].replace(/\s/g, "")
+                : ""
+
+              const address = {
+                street: street,
+                city: city,
+                state: this.getStateAddress(values),
+                postalCode: this.getPostalCode(values),
+                apt: values.apt ? values.apt : ""
+              }
+
+              localStorage.setItem("address", JSON.stringify(address))
+
+              if (address.postalCode === this.state.postalCode) {
+                window.firebase
+                  .auth()
+                  .currentUser.getIdToken(true)
+                  .then(idToken => {
+                    axios
+                      .put(
+                        `${API}/v1/subscribers`,
+                        {
+                          leadId: this.state.leadId,
+                          street: address.street,
+                          state: address.state,
+                          city: address.city
+                        },
+                        {
+                          headers: {
+                            Authorization: idToken
+                          }
                         }
-                      }
-                    )
-                    .then(() => {
-                      if (this.state.storedUtilityPaperOnly) {
-                        localStorage.setItem(
-                          "billingMethod",
-                          JSON.stringify({
-                            billingMethod: "paper"
-                          })
-                        )
-                        Router.push({
-                          pathname: "/onboarding/step5"
-                        })
-                      } else {
-                        localStorage.setItem(
-                          "billingMethod",
-                          JSON.stringify(values)
-                        )
+                      )
+                      .then(() => {
                         Router.push({
                           pathname: "/onboarding/step4"
                         })
-                      }
-                    })
-                    .catch(() => {})
+                      })
+                      .catch(() => {})
+                  })
+              } else {
+                this.setState({
+                  errorMessage:
+                    "Address has different zip code than the one initially provided."
                 })
+              }
             }}
             render={props => (
               <Form>
-                <Phoneinput
-                  value={props.values.phoneNumber}
-                  onChangeEvent={props.setFieldValue}
-                  onBlurEvent={props.setFieldTouched}
-                  label="Phone"
-                  fieldname="phoneNumber"
+                <GeoSuggest
+                  label="Address"
+                  fieldname="address"
+                  value={props.values.address}
+                  onChange={props.setFieldValue}
+                  onBlur={props.setFieldTouched}
+                  error={props.errors.topics}
+                  touched={props.touched.topics}
                 />
-                <Button primary disabled={!props.values.phoneNumber != ""}>
+                <p className="error">{this.state.errorMessage}</p>
+                <Button primary disabled={!props.values.address != ""}>
                   Next
                 </Button>
               </Form>
             )}
           />
           <Stepper>
-            <li className="steplist__step steplist__step-done">1</li>
-            <li className="steplist__step steplist__step-done">2</li>
-            <li className="steplist__step steplist__step-doing">3</li>
+            <li className="steplist__step steplist__step-doing">1</li>
+            <li className="steplist__step">2</li>
+            <li className="steplist__step">3</li>
             <li className="steplist__step">4</li>
             <li className="steplist__step">5</li>
             <li className="steplist__step">6</li>
@@ -137,6 +158,11 @@ class Step3 extends React.Component {
             height: 88vh;
             max-width: 700px;
             margin: 0 auto;
+          }
+          .error {
+            height: 45px;
+            color: red;
+            text-align: center;
           }
         `}</style>
       </main>
