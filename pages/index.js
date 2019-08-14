29b13f60df3c9d11f20419b2761727e22d4564f2
smartdev328 <solid.dev3028@gmail.com>
2019-08-14 10:21:11 -0400

    adding segment call when user signs in

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,26 @@
-import React from "react"
-import Router from "next/router"
-import { Formik, Form } from "formik"
-import axios from "axios"
-import SingleStep from "../components/SingleStep"
-import Header from "../components/Header"
-import Separator from "../components/Separator"
-import Input from "../components/Input"
-import Button from "../components/Button"
-import CONSTANTS from "../globals"
+import React from "react";
+import Router from "next/router";
+import { Formik, Form } from "formik";
+import axios from "axios";
+import SingleStep from "../components/SingleStep";
+import Header from "../components/Header";
+import Separator from "../components/Separator";
+import Input from "../components/Input";
+import Button from "../components/Button";
+import CONSTANTS from "../globals";
 
 const { API } =
-  CONSTANTS.NODE_ENV !== "production" ? CONSTANTS.dev : CONSTANTS.prod
+  CONSTANTS.NODE_ENV !== "production" ? CONSTANTS.dev : CONSTANTS.prod;
 
 class Step1 extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       error: {
         code: false,
         message: ""
       }
-    }
+    };
   }
 
   componentDidMount() {
@@ -38,7 +38,7 @@ class Step1 extends React.Component {
       .auth()
       .signInWithEmailAndPassword(values.emailAddress, values.password)
       .catch(error => {
-        this.setState({ error: { code: error.code, message: error.message } })
+        this.setState({ error: { code: error.code, message: error.message } });
       })
       .then(firebaseData => {
         if (!this.state.error.code) {
@@ -53,23 +53,27 @@ class Step1 extends React.Component {
                   }
                 })
                 .then(response => {
-                  const user = response.data.data
-                  window.localStorage.setItem("leadId", user.leadId)
+                  const user = response.data.data;
+                  window.localStorage.setItem("leadId", user.leadId);
+                  global.analytics.identify(user.leadId, {
+                    email: user.email
+                  });
+                  global.analytics.track("User Signed In", {});
 
                   if (user.signupCompleted) {
                     Router.push({
                       pathname: "/dashboard"
-                    })
+                    });
                   } else if (!user.phone) {
                     Router.push({
                       pathname: "/onboarding/step2",
                       query: {
                         onboardingNotFinished: true
                       }
-                    })
+                    });
                   } else if (!user.milestones.utilityInfoCompleted) {
-                    const utility = user.milestones.utility
-                    const imageName = utility.replace(/\s/g, "")
+                    const utility = user.milestones.utility;
+                    const imageName = utility.replace(/\s/g, "");
                     const utilityInfo = {
                       image: {
                         src: imageName
@@ -78,26 +82,29 @@ class Step1 extends React.Component {
                         altText: "Utility logo"
                       },
                       label: utility
-                    }
-                    localStorage.setItem("utility", JSON.stringify(utilityInfo))
+                    };
+                    localStorage.setItem(
+                      "utility",
+                      JSON.stringify(utilityInfo)
+                    );
                     if (user.milestones.utilityPaperOnly) {
                       localStorage.setItem(
                         "billingMethod",
                         JSON.stringify({
                           billingMethod: "paper"
                         })
-                      )
+                      );
 
                       Router.push({
                         pathname: "/onboarding/step8"
-                      })
+                      });
                     } else {
                       Router.push({
                         pathname: "/onboarding/step5",
                         query: {
                           onboardingNotFinished: true
                         }
-                      })
+                      });
                     }
                   } else if (!user.milestones.bankInfoCompleted) {
                     Router.push({
@@ -105,17 +112,17 @@ class Step1 extends React.Component {
                       query: {
                         onboardingNotFinished: true
                       }
-                    })
+                    });
                   }
                 })
-                .catch(() => {})
-            })
+                .catch(() => {});
+            });
         }
-      })
+      });
   }
 
   static getInitialProps({ query }) {
-    return { query }
+    return { query };
   }
 
   render() {
@@ -129,7 +136,7 @@ class Step1 extends React.Component {
               password: ""
             }}
             onSubmit={values => {
-              this.autenticate(values)
+              this.autenticate(values);
             }}
             render={props => (
               <React.Fragment>
@@ -160,7 +167,7 @@ class Step1 extends React.Component {
                           code: false,
                           message: ""
                         }
-                      })
+                      });
                     }}
                   >
                     Sign in
@@ -180,7 +187,7 @@ class Step1 extends React.Component {
             onClick={() => {
               Router.push({
                 pathname: "/onboarding/step1"
-              })
+              });
             }}
           >
             Sign Up
@@ -206,8 +213,8 @@ class Step1 extends React.Component {
           }
         `}</style>
       </main>
-    )
+    );
   }
 }
 
-export default Step1
+export default Step1;
