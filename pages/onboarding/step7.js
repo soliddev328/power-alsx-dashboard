9367504d3d06224fcbc6a848ba0f53d14f1aaca7
Robert Zhang <solid.dev3028@gmail.com>
2019-09-05 00:11:22 +0200

    add new flow

--- pages/onboarding/step7.js
+++ pages/onboarding/step7.js
@@ -1,95 +1,272 @@
 import React from "react"
-import Router from "next/router"
+import { Formik, Form } from "formik"
+import axios from "axios"
+import { Elements, StripeProvider } from "react-stripe-elements"
 import Header from "../../components/Header"
-import SingleStep from "../../components/SingleStep"
+import Input from "../../components/Input"
 import Button from "../../components/Button"
+import SingleStep from "../../components/SingleStep"
+import Plaid from "../../components/Plaid"
+import Checkout from "../../components/Checkout"
+import Stepper from "../../components/Stepper"
+import CONSTANTS from "../../globals"
+import Router from "next/router"
+
+const { STRIPE_KEY, API } =
+  CONSTANTS.NODE_ENV !== "production" ? CONSTANTS.dev : CONSTANTS.prod
 
 class Step7 extends React.Component {
   constructor(props) {
     super(props)
 
-    this.state = {}
+    this.state = {
+      name: "",
+      email: "",
+      errorMessage: "",
+      paymentMethod: "",
+      stripe: null
+    }
   }
 
   componentDidMount() {
     global.analytics.page("Step 7")
 
-    let storedPartialConnection = false
+    let storedPaymentMethod = ""
+    let storedLeadId = ""
 
-    if (window.localStorage.getItem("partialConnection")) {
-      storedPartialConnection = JSON.parse(
-        window.localStorage.getItem("partialConnection")
-      )
+    if (localStorage.getItem("paymentMethod")) {
+      storedPaymentMethod = JSON.parse(localStorage.getItem("paymentMethod"))
+    }
+
+    if (localStorage.getItem("leadId")) {
+      storedLeadId = localStorage.getItem("leadId")
     }
 
     this.setState({
-      partialConnection: storedPartialConnection
+      paymentMethod: storedPaymentMethod.paymentMethod,
+      leadId: storedLeadId,
+      stripe: window.Stripe(STRIPE_KEY)
     })
   }
 
-  renderContent() {
-    if (this.state.partialConnection) {
-      return (
-        <p className="message">
-          We are connecting your account and will contact you if we need more
-          information
-          <style jsx>{`
-            .message {
-              text-align: center;
-            }
-          `}</style>
-        </p>
-      )
+  renderBankLink() {
+    return (
+      <SingleStep>
+        <Plaid />
+        <style jsx>{`
+          .container {
+            display: grid;
+            grid-template-columns: repeat(auto-fill, minmax(140px, 1fr));
+            grid-column-gap: 15px;
+            grid-row-gap: 20px;
+            justify-items: center;
+          }
+        `}</style>
+      </SingleStep>
+    )
+  }
+
+  renderCreditCard() {
+    return (
+      <SingleStep title="Please enter your credit card information">
+        <div className="container">
+          <img className="cards" src="/static/images/banks/cards.png" alt="" />
+          <StripeProvider stripe={this.state.stripe}>
+            <Elements>
+              <Checkout stripe={this.state.stripe} name={this.props.name} />
+            </Elements>
+          </StripeProvider>
+        </div>
+        <style jsx>{`
+          p {
+            text-align: center;
+            vertical-align: middle;
+          }
+
+          p.small {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            font-size: 12px;
+          }
+
+          p svg {
+            margin-right: 10px;
+          }
+
+          em .highlight {
+            color: #2479ff;
+            font-weight: 700;
+          }
+
+          .cards {
+            display: block;
+            max-width: 35%;
+            margin: 0 auto;
+            margin-bottom: 1em;
+          }
+
+          .container h5 {
+            display: inline-block;
+            font-size: 1rem;
+            font-weight: 700;
+            position: relative;
+            color: #ff69a0;
+          }
+
+          .container h5 svg {
+            position: absolute;
+            right: 0;
+            bottom: 0;
+            transform: translate(50%, 30%);
+          }
+        `}</style>
+        <Stepper>
+          <li className="steplist__step steplist__step-done">1</li>
+          <li className="steplist__step steplist__step-done">2</li>
+          <li className="steplist__step steplist__step-done">3</li>
+          <li className="steplist__step steplist__step-done">4</li>
+          <li className="steplist__step steplist__step-doing">5</li>
+        </Stepper>
+      </SingleStep>
+    )
+  }
+
+  updateInfo(values) {
+    if (values.bankAccountNumber !== values.bankAccountNumberConfirmation) {
+      this.setState({
+        errorMessage: "Bank account number and confirmation don't match"
+      })
+    } else if (values.bankRoutingNumber.length !== 9) {
+      this.setState({
+        errorMessage: "Bank Routing Number should have 9 digits"
+      })
+    } else if (values.bankAccountNumber.length < 4) {
+      this.setState({
+        errorMessage: "Bank Account Number should have at least 4 digits"
+      })
     } else {
-      return (
-        <React.Fragment>
-          <svg width="55" height="55" xmlns="http://www.w3.org/2000/svg">
-            <g fill="none" fillRule="evenodd">
-              <circle cx="27.5" cy="27.5" r="27.5" fill="#41EF8B" />
-              <path
-                d="M32.9977909 21.3976621c1.6389735-1.6389735 4.2050581.8556542 2.494699 2.494699l-10.262155 10.333588c-.712883.6414974-1.7817447.6414974-2.494699 0l-5.1310774-5.2025105c-1.6389731-1.6389736.8556542-4.1336251 2.4946989-2.494699l3.9195632 3.9195632 8.9789703-9.0506407z"
-                fill="#FFF"
+      window.firebase
+        .auth()
+        .currentUser.getIdToken(true)
+        .then(idToken => {
+          axios
+            .put(
+              `${API}/v1/subscribers`,
+              {
+                leadId: this.state.leadId,
+                bank: values.bankName,
+                bankRoutingNumber: values.bankRoutingNumber,
+                bankAccountNumber: values.bankAccountNumber
+              },
+              {
+                headers: {
+                  Authorization: idToken
+                }
+              }
+            )
+            .then(() => {
+              global.analytics.track("Sign-Up Completed", {})
+              localStorage.setItem("usercreated", true)
+              Router.push({
+                pathname: "/dashboard"
+              })
+            })
+            .catch(() => {})
+        })
+    }
+  }
+
+  renderManualDataInsert() {
+    return (
+      <SingleStep title="Please provide the following information.">
+        <Formik
+          initialValues={{
+            bankName: "",
+            bankRoutingNumber: "",
+            bankAccountNumber: "",
+            bankAccountNumberConfirmation: ""
+          }}
+          onSubmit={values => {
+            this.updateInfo(values)
+          }}
+          render={props => (
+            <Form>
+              <Input type="text" label="Bank Name" fieldname="bankName" />
+              <Input
+                type="text"
+                label="Bank Routing Number (9 digits)"
+                fieldname="bankRoutingNumber"
+                maxLength="9"
               />
-            </g>
-          </svg>
-          <p>Congratulations, you're connected!</p>
-        </React.Fragment>
-      )
+              <Input
+                type="text"
+                label="Bank Account Number (4 digits min)"
+                fieldname="bankAccountNumber"
+              />
+              <Input
+                type="text"
+                label="Bank Account Number Confirmation"
+                fieldname="bankAccountNumberConfirmation"
+              />
+              <p className="error">{this.state.errorMessage}</p>
+
+              <Button
+                primary
+                disabled={
+                  !props.values.bankName != "" ||
+                  !props.values.bankRoutingNumber != "" ||
+                  !props.values.bankAccountNumber != "" ||
+                  !props.values.bankAccountNumberConfirmation != ""
+                }
+              >
+                Next
+              </Button>
+            </Form>
+          )}
+        />
+        <style jsx>{`
+          .error {
+            height: 23px;
+            color: red;
+            text-align: center;
+          }
+        `}</style>
+        <Stepper>
+          <li className="steplist__step steplist__step-done">1</li>
+          <li className="steplist__step steplist__step-done">2</li>
+          <li className="steplist__step steplist__step-done">3</li>
+          <li className="steplist__step steplist__step-done">4</li>
+          <li className="steplist__step steplist__step-doing">5</li>
+        </Stepper>
+      </SingleStep>
+    )
+  }
+
+  renderForm() {
+    const { paymentMethod } = this.state
+    if (paymentMethod === "automatic") {
+      return this.renderBankLink()
+    } else if (paymentMethod === "creditCard") {
+      return this.renderCreditCard()
+    } else {
+      return this.renderManualDataInsert()
     }
   }
 
   render() {
+    const { paymentMethod } = this.state
     return (
       <main>
         <Header />
-        <SingleStep>
-          <div className="loading">{this.renderContent()}</div>
-          <Button
-            primary
-            onClick={() => {
-              Router.push({
-                pathname: "/onboarding/step8"
-              })
-            }}
-          >
-            Next
-          </Button>
-        </SingleStep>
+        {paymentMethod && this.renderForm()}
+
         <style jsx>{`
           main {
             display: block;
             height: 88vh;
             max-width: 700px;
             margin: 0 auto;
-          }
-          .loading {
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            justify-content: center;
-          }
-          h3 {
-            text-align: center;
           }
         `}</style>
       </main>
