ff3eb9d3bb3b3964f44b1a47667d28780d493cb2
smartdev328 <solid.dev3028@gmail.com>
2019-09-05 00:11:22 +0200

    add new flow

--- pages/onboarding/step4.js
+++ pages/onboarding/step4.js
@@ -1,119 +1,417 @@
-import React from "react";
-import Router from "next/router";
-import { Formik, Form } from "formik";
-import axios from "axios";
-import Phoneinput from "../../components/Phoneinput";
-import Header from "../../components/Header";
-import SingleStep from "../../components/SingleStep";
-import Button from "../../components/Button";
-import Stepper from "../../components/Stepper";
-import CONSTANTS from "../../globals";
+import React from "react"
+import Router from "next/router"
+import { Formik, Form } from "formik"
+import axios from "axios"
+import { FadeLoader } from "react-spinners"
+import Header from "../../components/Header"
+import Input from "../../components/Input"
+import Checkbox from "../../components/Checkbox"
+import SingleStep from "../../components/SingleStep"
+import Button from "../../components/Button"
+import Stepper from "../../components/Stepper"
+import CONSTANTS from "../../globals"
 
 const { API } =
-  CONSTANTS.NODE_ENV !== "production" ? CONSTANTS.dev : CONSTANTS.prod;
+  CONSTANTS.NODE_ENV !== "production" ? CONSTANTS.dev : CONSTANTS.prod
 
 class Step4 extends React.Component {
   constructor(props) {
-    super(props);
+    super(props)
+
+    this.state = {
+      isLoading: false,
+      currentUtility: "",
+      forgotPwdLink: "",
+      forgotEmailLink: "",
+      createLoginLink: "",
+      agreement: {
+        terms: "",
+        conditions: ""
+      }
+    }
+  }
+
+  static async getInitialProps({ req, query, params }) {
+    if (req) {
+      try {
+        return { query: req.query, params: req.params }
+      } catch (err) {
+        return { query: req.query, params: req.params }
+      }
+    }
+
+    return { query, params }
+  }
+
+  getLinks() {
+    let storedAddress = JSON.parse(localStorage.getItem("address"))
+    let storedUtility = JSON.parse(localStorage.getItem("utility"))
+
+    if (storedUtility && storedAddress) {
+      const rawParams = {
+        utility: encodeURIComponent(storedUtility.label),
+        state: storedAddress.state
+      }
+
+      const generatedParams = Object.entries(rawParams)
+        .map(([key, val]) => `${key}=${val}`)
+        .join("&")
+
+      axios(`${API}/v1/utilities/?${generatedParams}`).then(response => {
+        if (response.data.data) {
+          const data = response.data.data[0]
+
+          this.setState({
+            forgotPwdLink: data.forgotPwdLink,
+            forgotEmailLink: data.forgotEmailLink,
+            createLoginLink: data.createLoginLink,
+            agreement: {
+              terms: data.agreement.termsLink,
+              conditions: data.agreement.conditionsLink
+            }
+          })
+        }
+      })
+    }
   }
 
   componentDidMount() {
-    global.analytics.page("Step 4");
+    global.analytics.page("Step 4")
 
-    let storedLeadId = "";
-    let storedUtilityPaperOnly = false;
+    let storedLeadId = ""
+    let storedUtility = ""
+    let storedBillingMethod = ""
 
     if (localStorage.getItem("leadId")) {
-      storedLeadId = localStorage.getItem("leadId");
+      storedLeadId = localStorage.getItem("leadId")
     }
 
     if (localStorage.getItem("utility")) {
-      let storedUtility = JSON.parse(localStorage.getItem("utility"));
-      if (storedUtility && storedUtility.paperOnly)
-        storedUtilityPaperOnly = true;
+      storedUtility = JSON.parse(localStorage.getItem("utility"))
     }
 
-    this.setState({
-      leadId: storedLeadId,
-      storedUtilityPaperOnly
-    });
+    if (localStorage.getItem("billingMethod")) {
+      storedBillingMethod = JSON.parse(localStorage.getItem("billingMethod"))
+    }
+
+    this.setState(
+      {
+        leadId: storedLeadId,
+        utility: storedUtility.label,
+        currentUtility: storedUtility,
+        billingMethod: storedBillingMethod
+      },
+      this.getLinks()
+    )
   }
 
-  render() {
+  renderUtilityLogin() {
+    const { query } = this.props
     return (
-      <main>
-        <Header />
-        <SingleStep title="And what phone number would you like to use with the account?">
-          <Formik
-            initialValues={{
-              phoneNumber: ""
-            }}
-            onSubmit={values => {
-              localStorage.setItem("phoneNumer", values.phoneNumber);
-              window.firebase
-                .auth()
-                .currentUser.getIdToken(true)
-                .then(idToken => {
-                  axios
-                    .put(
-                      `${API}/v1/subscribers`,
-                      {
-                        leadId: this.state.leadId,
-                        phone: values.phoneNumber
-                      },
-                      {
-                        headers: {
-                          Authorization: idToken
-                        }
+      <React.Fragment>
+        {query && query.error && (
+          <p className="error">
+            There was a problem connecting, could you please verify your login.
+          </p>
+        )}
+        <Formik
+          initialValues={{
+            utilityUser: "",
+            utilityPassword: ""
+          }}
+          onSubmit={values => {
+            this.setState({ isLoading: true })
+            window.firebase
+              .auth()
+              .currentUser.getIdToken(true)
+              .then(idToken => {
+                axios
+                  .put(
+                    `${API}/v1/subscribers/utilities/link`,
+                    {
+                      leadId: this.state.leadId,
+                      utility: this.state.utility,
+                      agreementChecked: !!values.acceptedTermsAndConditions,
+                      utilityUsername: values.utilityUser,
+                      utilityPwd: values.utilityPassword
+                    },
+                    {
+                      headers: {
+                        Authorization: idToken
                       }
-                    )
-                    .then(() => {
-                      if (this.state.storedUtilityPaperOnly) {
-                        localStorage.setItem(
-                          "billingMethod",
-                          JSON.stringify({
-                            billingMethod: "paper"
-                          })
-                        );
-                        Router.push({
-                          pathname: "/onboarding/step6"
-                        });
-                      } else {
-                        localStorage.setItem(
-                          "billingMethod",
-                          JSON.stringify(values)
-                        );
+                    }
+                  )
+                  .then(response => {
+                    const data = response.data.data
+
+                    localStorage.setItem("linkedUtility", JSON.stringify(data))
+
+                    if (data && data[0]) {
+                      if (data[0].hasLoggedIn) {
+                        localStorage.setItem("partialConnection", false)
                         Router.push({
                           pathname: "/onboarding/step5"
-                        });
+                        })
+                      } else {
+                        this.setState({ isLoading: false })
+                        Router.push({
+                          pathname: "/onboarding/step4",
+                          query: {
+                            error: true
+                          }
+                        })
                       }
-                    })
-                    .catch(() => {});
-                });
-            }}
-            render={props => (
+                    } else {
+                      localStorage.setItem("partialConnection", true)
+                      Router.push({
+                        pathname: "/onboarding/step5"
+                      })
+                    }
+                  })
+                  .catch(err => {
+                    console.log(err)
+                  })
+              })
+          }}
+          render={props => (
+            <React.Fragment>
               <Form>
-                <Phoneinput
-                  value={props.values.phoneNumber}
-                  onChangeEvent={props.setFieldValue}
-                  onBlurEvent={props.setFieldTouched}
-                  label="Phone"
-                  fieldname="phoneNumber"
+                <Input label="User name" fieldname="utilityUser" />
+                <Input
+                  type="password"
+                  label="Password"
+                  fieldname="utilityPassword"
+                  autoComplete="no"
                 />
-                <Button primary disabled={!props.values.phoneNumber != ""}>
+                <Checkbox fieldname="acceptedTermsAndConditions">
+                  <p className="checkbox__label">
+                    I authorize Common Energy to act as my Agent and to enroll
+                    me in a clean energy savings program, according to these{" "}
+                    <a
+                      href={this.state.agreement.terms}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      terms
+                    </a>{" "}
+                    and{" "}
+                    <a
+                      href={this.state.agreement.conditions}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      conditions
+                    </a>
+                    .
+                  </p>
+                </Checkbox>
+                <Button
+                  primary
+                  disabled={
+                    !props.values.acceptedTermsAndConditions != "" ||
+                    !props.values.utilityUser != "" ||
+                    !props.values.utilityPassword != ""
+                  }
+                >
                   Next
                 </Button>
               </Form>
-            )}
+            </React.Fragment>
+          )}
+        />
+        <style jsx>{`
+          .error {
+            text-align: center;
+            color: red;
+          }
+        `}</style>
+      </React.Fragment>
+    )
+  }
+
+  renderAskForUtilityAccount() {
+    return (
+      <Formik
+        initialValues={{
+          utilityAccountNumber: "",
+          acceptedTermsAndConditions: false
+        }}
+        onSubmit={values => {
+          window.firebase
+            .auth()
+            .currentUser.getIdToken(true)
+            .then(idToken => {
+              axios
+                .put(
+                  `${API}/v1/subscribers`,
+                  {
+                    leadId: this.state.leadId,
+                    agreementChecked: !!values.acceptedTermsAndConditions,
+                    utilityAccountNumber: values.utilityAccountNumber
+                  },
+                  {
+                    headers: {
+                      Authorization: idToken
+                    }
+                  }
+                )
+                .then(() => {
+                  localStorage.setItem("partialConnection", true)
+                  Router.push({
+                    pathname: "/onboarding/step4.2"
+                  })
+                })
+                .catch(error => {
+                  console.log(error)
+                })
+            })
+        }}
+        render={props => (
+          <React.Fragment>
+            <Form>
+              <Input label="Account Number" fieldname="utilityAccountNumber" />
+              <Checkbox fieldname="acceptedTermsAndConditions">
+                <p className="checkbox__label">
+                  I authorize Common Energy to act as my Agent and to enroll me
+                  in a clean energy savings program, according to these{" "}
+                  <a
+                    href={this.state.agreement.terms}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    terms
+                  </a>{" "}
+                  and{" "}
+                  <a
+                    href={this.state.agreement.conditions}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    conditions
+                  </a>
+                  .
+                </p>
+              </Checkbox>
+              <Button
+                primary
+                disabled={
+                  !props.values.utilityAccountNumber != "" ||
+                  !props.values.acceptedTermsAndConditions
+                }
+              >
+                Next
+              </Button>
+            </Form>
+          </React.Fragment>
+        )}
+      />
+    )
+  }
+
+  renderForms() {
+    const canLinkAccount =
+      this.state.billingMethod &&
+      this.state.billingMethod.billingMethod.indexOf("paper") !== 0
+
+    return canLinkAccount
+      ? this.renderUtilityLogin()
+      : this.renderAskForUtilityAccount()
+  }
+
+  renderLoader() {
+    return (
+      <React.Fragment>
+        <div className="loading">
+          <FadeLoader
+            className="spinner"
+            height={15}
+            width={4}
+            radius={1}
+            color={"#FF69A0"}
+            loading={true}
           />
-          <Stepper>
-            <li className="steplist__step steplist__step-done">1</li>
-            <li className="steplist__step steplist__step-done">2</li>
-            <li className="steplist__step steplist__step-doing">3</li>
-            <li className="steplist__step">4</li>
-            <li className="steplist__step">5</li>
-            <li className="steplist__step">6</li>
-          </Stepper>
+          <p>Connecting your account</p>
+        </div>
+        <p className="suffix">(this may take up to 10 seconds)</p>
+        <style jsx>{`
+          .loading {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+          }
+
+          .loading p {
+            font-size: 1rem;
+            text-align: center;
+          }
+
+          p.suffix {
+            font-size: 0.8rem;
+          }
+        `}</style>
+      </React.Fragment>
+    )
+  }
+
+  renderText() {
+    const canLinkAccount =
+      this.state.billingMethod &&
+      this.state.billingMethod.billingMethod.indexOf("paper") !== 0
+    let text = canLinkAccount
+      ? "Ok great. Let's connect your account and get you saving!"
+      : "No problem! We can use your account number to get you connected and saving."
+
+    if (this.state.currentUtility && this.state.currentUtility.paperOnly)
+      text = "We can use your account number to get you connected and saving."
+
+    return this.state.isLoading ? "" : text
+  }
+
+  render() {
+    const canLinkAccount =
+      this.state.billingMethod &&
+      this.state.billingMethod.billingMethod.indexOf("paper") !== 0
+    return (
+      <main>
+        <Header />
+        <SingleStep title={this.renderText()}>
+          {this.state && this.state.currentUtility && !this.state.isLoading && (
+            <figure>
+              <img
+                src={this.state.currentUtility.image.src}
+                alt={this.state.currentUtility.image.altText}
+              />
+            </figure>
+          )}
+          {this.state.isLoading ? this.renderLoader() : this.renderForms()}
+          {!this.state.isLoading && canLinkAccount && (
+            <div className="links">
+              {this.state.createLoginLink && (
+                <a className="cta" href={this.state.createLoginLink}>
+                  Create an account
+                </a>
+              )}
+              {this.state.forgotEmailLink && (
+                <a className="cta" href={this.state.forgotEmailLink}>
+                  Forgot username
+                </a>
+              )}
+              {this.state.forgotPwdLink && (
+                <a className="cta" href={this.state.forgotPwdLink}>
+                  Forgot password
+                </a>
+              )}
+            </div>
+          )}
+          {!this.state.isLoading && (
+            <Stepper>
+              <li className="steplist__step steplist__step-done">1</li>
+              <li className="steplist__step steplist__step-doing">2</li>
+              <li className="steplist__step">3</li>
+              <li className="steplist__step">4</li>
+              <li className="steplist__step">5</li>
+            </Stepper>
+          )}
         </SingleStep>
         <style jsx>{`
           main {
@@ -122,10 +420,30 @@ class Step4 extends React.Component {
             max-width: 700px;
             margin: 0 auto;
           }
+          figure {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            background-color: #fff;
+            margin: 0;
+            margin-bottom: 1em;
+            padding: 1em;
+          }
+
+          img {
+            max-width: 60%;
+          }
+
+          .links {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+          }
         `}</style>
       </main>
-    );
+    )
   }
 }
 
-export default Step4;
+export default Step4
