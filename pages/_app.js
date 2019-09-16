aa1bc7c7c033c035d6e50a6dbc94cfffb879a694
Robert Zhang <solid.dev3028@gmail.com>
2019-09-16 15:06:01 -0300

    remove deprecated container

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import App, { Container } from "next/app";
+import App from "next/app";
 import Head from "next/head";
 import React from "react";
 import settings from "../settings.json";
@@ -7,12 +7,12 @@ class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <Container>
+      <>
         <Head>
           <title>{settings.meta.pageTitle}</title>
         </Head>
         <Component {...pageProps} />
-      </Container>
+      </>
     );
   }
 }
