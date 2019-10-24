aac595c5b0a22d992ac422566ef5e2355a645074
Robert Zhang <solid.dev3028@gmail.com>
2019-10-24 11:40:14 -0300

    add messages and minor extra fixes

--- pages/dashboard/your-project.js
+++ pages/dashboard/your-project.js
@@ -9,6 +9,7 @@ import Panel from "../../components/Panel";
 import Image from "../../components/Image";
 import Text from "../../components/Text";
 import CONSTANTS from "../../globals";
+import { privateEncrypt } from "crypto";
 
 const { API } =
   CONSTANTS.NODE_ENV !== "production" ? CONSTANTS.dev : CONSTANTS.prod;
@@ -34,6 +35,7 @@ const getProjectInfo = async (projectId, idToken) => {
 export default function MySource() {
   const [userData, setUserdata] = useState({});
   const [isLoading, setIsLoading] = useState(true);
+  const [overlayDescription, setOverlayDescription] = useState(false);
   const [projectInfo, setProjectInfo] = useState({});
   const [{ selectedAccount }, dispatch] = useStateValue();
 
@@ -44,6 +46,42 @@ export default function MySource() {
         global.analytics.page("Your project");
         user.getIdToken(true).then(async idToken => {
           const userInfo = await getUserData(user.uid, idToken);
+
+          if (
+            userInfo.accounts[selectedAccount.value].onboardingStatus ===
+            "Unassigned"
+          ) {
+            setOverlayDescription(
+              "Please stay tuned to learn more about the status of your project and your connection timeline! We will provide more details shortly, both here and by email -- and please feel free to always reach out with any questions at"
+            );
+          } else if (
+            userInfo.accounts[selectedAccount.value].onboardingStatus ===
+            "Project Live"
+          ) {
+            setOverlayDescription(
+              "Common Energy can connect you to our available project in your community in as little as 2-5 weeks. We'll keep you posted on our progress along the way -- and please feel free to always reach out with any questions at"
+            );
+          } else if (
+            userInfo.accounts[selectedAccount.value].onboardingStatus ===
+            "No Project"
+          ) {
+            setOverlayDescription(
+              "Currently, all our projects in your area are filled. However, because of your interest, we're able to work with a developer on a new project to bring more clean energy and savings to your community! Please stand-by for an update on new projects in a few weeks -- and feel free to reach out with any questions at"
+            );
+          } else if (
+            userInfo.accounts[selectedAccount.value].onboardingStatus ===
+            "Project Not Live"
+          ) {
+            setOverlayDescription(
+              "We're excited to let you know that we have an available project in your area, but we are not ready to connect you, as it is not yet ready and active. However, we will make sure we update you as we take the project through to completion. Please know that as an early subscriber, you've taken an important step in making this project a success! Feel free to reach out with any questions at"
+            );
+          } else if (
+            userInfo.accounts[selectedAccount.value].onboardingStatus ===
+            "Meter Live"
+          ) {
+            setOverlayDescription(false);
+          }
+
           const projectData = await getProjectInfo(
             userInfo.accounts[selectedAccount.value].projectId,
             idToken
@@ -61,7 +99,7 @@ export default function MySource() {
   }, [selectedAccount.value]);
 
   return (
-    <Main isLoading={isLoading}>
+    <Main isLoading={isLoading && !overlayDescription}>
       <Head>
         <title>Common Energy - Your Project</title>
       </Head>
@@ -69,7 +107,10 @@ export default function MySource() {
         Your project
       </Text>
       <div className="inner">
-        <Section>
+        <Section
+          disabled={overlayDescription}
+          overlayDescription={overlayDescription}
+        >
           {projectInfo ? (
             <Image
               hasBorder
@@ -119,93 +160,97 @@ export default function MySource() {
             </Panel>
           )}
         </Section>
-        <Section disabled={!projectInfo}>
-          <Panel>
-            <Text h3>Project Summary</Text>
-            <div className="item">
-              <Text noMargin bold>
-                Project Name:
-              </Text>
-              <Text noMargin>{projectInfo ? `${projectInfo.name}` : ""}</Text>
-            </div>
+        {!overlayDescription && (
+          <Section disabled={!projectInfo}>
+            <Panel>
+              <Text h3>Project Summary</Text>
+              <div className="item">
+                <Text noMargin bold>
+                  Project Name:
+                </Text>
+                <Text noMargin>{projectInfo ? `${projectInfo.name}` : ""}</Text>
+              </div>
 
-            <div className="item">
-              <Text noMargin bold>
-                Project Address:
-              </Text>
-              <Text noMargin>
-                {projectInfo ? `${projectInfo.town}, ${projectInfo.state}` : ""}
-              </Text>
-            </div>
-            <div className="item">
-              <Text noMargin bold>
-                Project Size:
-              </Text>
-              <Text noMargin>
-                {projectInfo ? (
-                  <NumberFormat
-                    value={projectInfo.sizeDC}
-                    displayType={"text"}
-                    thousandSeparator={true}
-                    suffix={" kW DC"}
-                  />
-                ) : (
-                  ""
-                )}{" "}
-              </Text>
-            </div>
-            <div className="item">
-              <Text noMargin bold>
-                Annual generation:
-              </Text>
-              <Text noMargin>
-                {projectInfo ? (
-                  <NumberFormat
-                    value={projectInfo.annualGeneration}
-                    displayType={"text"}
-                    thousandSeparator={true}
-                    suffix={" kWh"}
-                  />
-                ) : (
-                  ""
-                )}{" "}
-              </Text>
-            </div>
-            <div className="item">
-              <Text noMargin bold>
-                Annual avoided CO2:
-              </Text>
-              <Text noMargin>
-                {projectInfo ? (
-                  <NumberFormat
-                    value={projectInfo.annualAvoidedC02}
-                    displayType={"text"}
-                    thousandSeparator={true}
-                    suffix={" pounds"}
-                  />
-                ) : (
-                  ""
-                )}{" "}
-              </Text>
-            </div>
-            <div className="item">
-              <Text noMargin bold>
-                Equivalent trees planted:
-              </Text>
-              <Text noMargin>
-                {projectInfo ? (
-                  <NumberFormat
-                    value={projectInfo.annualTreesPlanted}
-                    displayType={"text"}
-                    thousandSeparator={true}
-                  />
-                ) : (
-                  ""
-                )}
-              </Text>
-            </div>
-          </Panel>
-        </Section>
+              <div className="item">
+                <Text noMargin bold>
+                  Project Address:
+                </Text>
+                <Text noMargin>
+                  {projectInfo
+                    ? `${projectInfo.town}, ${projectInfo.state}`
+                    : ""}
+                </Text>
+              </div>
+              <div className="item">
+                <Text noMargin bold>
+                  Project Size:
+                </Text>
+                <Text noMargin>
+                  {projectInfo ? (
+                    <NumberFormat
+                      value={projectInfo.sizeDC}
+                      displayType={"text"}
+                      thousandSeparator={true}
+                      suffix={" kW DC"}
+                    />
+                  ) : (
+                    ""
+                  )}{" "}
+                </Text>
+              </div>
+              <div className="item">
+                <Text noMargin bold>
+                  Annual generation:
+                </Text>
+                <Text noMargin>
+                  {projectInfo ? (
+                    <NumberFormat
+                      value={projectInfo.annualGeneration}
+                      displayType={"text"}
+                      thousandSeparator={true}
+                      suffix={" kWh"}
+                    />
+                  ) : (
+                    ""
+                  )}{" "}
+                </Text>
+              </div>
+              <div className="item">
+                <Text noMargin bold>
+                  Annual avoided CO2:
+                </Text>
+                <Text noMargin>
+                  {projectInfo ? (
+                    <NumberFormat
+                      value={projectInfo.annualAvoidedC02}
+                      displayType={"text"}
+                      thousandSeparator={true}
+                      suffix={" pounds"}
+                    />
+                  ) : (
+                    ""
+                  )}{" "}
+                </Text>
+              </div>
+              <div className="item">
+                <Text noMargin bold>
+                  Equivalent trees planted:
+                </Text>
+                <Text noMargin>
+                  {projectInfo ? (
+                    <NumberFormat
+                      value={projectInfo.annualTreesPlanted}
+                      displayType={"text"}
+                      thousandSeparator={true}
+                    />
+                  ) : (
+                    ""
+                  )}
+                </Text>
+              </div>
+            </Panel>
+          </Section>
+        )}
       </div>
       <style jsx>{`
         .item {
