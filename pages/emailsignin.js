60586fa10587c080741cfc04accc14079237c1de
Robert Zhang <solid.dev3028@gmail.com>
2019-12-13 15:54:20 -0300

    add email secure login ling

---
+++ pages/emailsignin.js
@@ -0,0 +1,79 @@
+import { useState, useEffect } from "react";
+import { Formik, Form } from "formik";
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Input from "../components/Input";
+import SingleStep from "../components/SingleStep";
+
+const EmailLogin = props => {
+  const [email, setEmail] = useState("");
+  const [emailSent, setEmailSent] = useState(false);
+
+  useEffect(() => {
+    setEmail(window.localStorage.getItem("emailAddress"));
+  }, []);
+
+  const actionCodeSettings = {
+    url: "https://my.commonenergy.us/emailsignin",
+    handleCodeInApp: true,
+    dynamicLinkDomain: "example.page.link"
+  };
+
+  const sendSecureLoginLink = values => {
+    const { email } = values;
+    window.firebase
+      .auth()
+      .sendSignInLinkToEmail(email, actionCodeSettings)
+      .then(() => {
+        setEmailSent(true);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
+
+  return (
+    <main>
+      <Header first />
+      <SingleStep
+        prefix={
+          emailSent
+            ? "An secure login link has been sent, check your inbox"
+            : "Enter the email that you used when you signed up for Common Energy and we will send you an email with a secure login link."
+        }
+      >
+        {!emailSent && email && (
+          <Formik
+            initialValues={{
+              email: email
+            }}
+            onSubmit={values => {
+              sendSecureLoginLink(values);
+            }}
+          >
+            {props => (
+              <>
+                <Form>
+                  <Input type="email" label="Email address" fieldname="email" />
+                  <Button primary disabled={!!props.values.email !== true}>
+                    Send secure login link
+                  </Button>
+                </Form>
+              </>
+            )}
+          </Formik>
+        )}
+      </SingleStep>
+      <style jsx>{`
+        main {
+          display: block;
+          height: 88vh;
+          max-width: 700px;
+          margin: 0 auto;
+        }
+      `}</style>
+    </main>
+  );
+};
+
+export default EmailLogin;
