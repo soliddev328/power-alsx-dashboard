c5044a9d7049beeb9e0e8c9a75cae45f85e01a1f
Robert Zhang <solid.dev3028@gmail.com>
2019-12-18 20:14:20 -0300

    cherry pick text changes from flow branch

--- components/Dashboard/ReferralsTable.js
+++ components/Dashboard/ReferralsTable.js
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import cn from "classnames";
 import Section from "../Section";
 import Panel from "../Panel";
 import Table from "../Table";
@@ -7,13 +6,17 @@ import Text from "../Text";
 
 const mapDetails = data => {
   const allItems = [];
-  data.forEach(element => {
+  data.contacts.forEach(element => {
     const singleItem = [];
     singleItem.push(element.name);
     singleItem.push(element.status);
-    singleItem.push(
-      element.createdDate ? element.createdDate : element.convertedDate
-    );
+    allItems.push(singleItem);
+  });
+
+  data.leads.forEach(element => {
+    const singleItem = [];
+    singleItem.push(element.name);
+    singleItem.push(element.status);
     allItems.push(singleItem);
   });
 
@@ -21,82 +24,24 @@ const mapDetails = data => {
 };
 
 export default function ReferralsTable({ data }) {
-  const [displayEnrolled, setDisplayEnrolled] = useState(true);
   const [contacts, setContacts] = useState([]);
-  const [leads, setLeads] = useState([]);
 
   useEffect(() => {
-    setLeads(mapDetails(data.leads));
-    setContacts(mapDetails(data.contacts));
+    setContacts(mapDetails(data));
   }, []);
 
   return (
     <Section tableSection>
       <Panel>
-        <Text h3>Your referrals</Text>
-        <div className="toggle-wrapper">
-          <button
-            className={cn("toggle", { active: displayEnrolled })}
-            onClick={() => {
-              setDisplayEnrolled(true);
-            }}
-          >
-            <Text bold style={{ color: "#000" }}>
-              Enrolled
-            </Text>
-          </button>
-          <button
-            className={cn("toggle", { active: !displayEnrolled })}
-            onClick={() => {
-              setDisplayEnrolled(false);
-            }}
-          >
-            <Text bold style={{ color: "#000" }}>
-              In Process
-            </Text>
-          </button>
-        </div>
-        {displayEnrolled ? (
-          contacts.length > 0 ? (
-            <Table headers={["Name", "Status", "Date"]} data={contacts} />
-          ) : (
-            <Text>
-              You have not made any referrals yet. Go ahead, give it a try! :)
-            </Text>
-          )
-        ) : leads.length > 0 ? (
-          <Table headers={["Name", "Status", "Date"]} data={leads} />
+        <Text h3>Your Referrals</Text>
+        {contacts.length > 0 ? (
+          <Table headers={["Name", "Status"]} data={contacts} />
         ) : (
           <Text>
             You have not made any referrals yet. Go ahead, give it a try! :)
           </Text>
         )}
       </Panel>
-      <style jsx>{`
-        .toggle-wrapper {
-          display: flex;
-          align-items: center;
-          justify-content: flex-end;
-          margin-bottom: 20px;
-        }
-        .toggle {
-          appearance: none;
-          background: none;
-          border: none;
-          border-radius: 0;
-          margin: 0;
-          padding-left: 0;
-          padding-right: 0;
-          cursor: pointer;
-          border-bottom: 2px solid var(--color-primary);
-        }
-        .toggle + .toggle {
-          margin-left: 20px;
-        }
-        .toggle:not(.active) {
-          opacity: 0.4;
-        }
-      `}</style>
     </Section>
   );
 }
