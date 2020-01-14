9764d1cefc7a2196e26cf01e5378fcc54d27f0a3
smartdev328 <solid.dev3028@gmail.com>
2020-01-14 12:36:48 -0300

    add missing arrows and fix mocked data

--- components/Dashboard/EmissionsChart.js
+++ components/Dashboard/EmissionsChart.js
@@ -1,8 +1,9 @@
 import { useState, useEffect } from "react";
-import { BarChart, Bar, YAxis, CartesianGrid } from "recharts";
+import { BarChart, Bar, YAxis, CartesianGrid, LabelList } from "recharts";
 import Text from "../Text";
 import Section from "../Section";
 import { useStateValue } from "../../state";
+import ArrowIcon from "../Icons/ArrowIcon";
 import axios from "axios";
 import CONSTANTS from "../../globals";
 
@@ -33,6 +34,20 @@ const customLabel = ({ x, y, value }) => (
   </text>
 );
 
+const labelListCustom = ({ x, y, value }) => (
+  <text
+    x="50%"
+    y="7%"
+    dy={-10}
+    fill="#555e80"
+    fontSize={12}
+    fontFamily="Poppins"
+    textAnchor="middle"
+  >
+    {value} lbs/MWh
+  </text>
+);
+
 function EmissionsChart() {
   const [emissionsInfo, setEmissionsInfo] = useState();
   const [{ selectedAccount }] = useStateValue();
@@ -43,7 +58,7 @@ function EmissionsChart() {
         user.getIdToken(true).then(async idToken => {
           const userInfo = await getUserData(user.uid, idToken);
           const { emissions } = userInfo.accounts[selectedAccount.value];
-          setEmissionsInfo(emissions.co2 * 0.1);
+          setEmissionsInfo(emissions?.co2 * 0.1);
         });
       }
     });
@@ -52,14 +67,14 @@ function EmissionsChart() {
   const data0 = [
     {
       name: "Utility's Emissions",
-      co2: emissionsInfo,
+      co2: 295,
       amt: 300
     }
   ];
 
   const data1 = [
     {
-      name: "Page A",
+      name: "Your emissions",
       co2: 30,
       amt: 300
     }
@@ -70,28 +85,60 @@ function EmissionsChart() {
       <Section columns="2" noMargin>
         <aside>
           <Text style={{ textAlign: "center" }}>Your Utility’s Emissions</Text>
-          <BarChart width={150} height={280} data={data0}>
+          <BarChart
+            margin={{ top: 35, right: 0, bottom: 0, left: 0 }}
+            width={150}
+            height={280}
+            data={data0}
+          >
             <CartesianGrid vertical={false} />
             <YAxis dataKey="amt" hide />
-            <Bar label={customLabel} dataKey="co2" fill="#113a7a" />
+            <Bar label={customLabel} dataKey="co2" fill="#113a7a">
+              <LabelList
+                content={labelListCustom}
+                dataKey="co2"
+                position="top"
+              />
+            </Bar>
           </BarChart>
+          <div className="arrow">
+            <ArrowIcon color="#2479ff" width="27" height="26" />
+          </div>
         </aside>
         <aside>
           <Text style={{ textAlign: "center" }}>
             Your Emissions with Common Energy
           </Text>
-          <BarChart width={150} height={280} data={data1}>
+          <BarChart
+            margin={{ top: 35, right: 0, bottom: 0, left: 0 }}
+            width={150}
+            height={280}
+            data={data1}
+          >
             <CartesianGrid vertical={false} />
             <YAxis dataKey="amt" hide />
-            <Bar label={customLabel} dataKey="co2" fill="#113a7a" />
+            <Bar label={customLabel} dataKey="co2" fill="#113a7a">
+              <LabelList
+                content={labelListCustom}
+                dataKey="co2"
+                position="top"
+              />
+            </Bar>
           </BarChart>
         </aside>
       </Section>
       <style jsx>{`
         aside {
           display: grid;
-          grid-template-rows: 70px 1fr;
+          grid-template-rows: 100px 1fr;
           justify-items: center;
+          position: relative;
+        }
+
+        .arrow {
+          position: absolute;
+          right: -18%;
+          top: 60%;
         }
       `}</style>
     </div>
