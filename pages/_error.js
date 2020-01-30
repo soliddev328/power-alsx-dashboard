17ffbd482928e76d01e26be9b40559938de71e05
Robert Zhang <solid.dev3028@gmail.com>
2020-01-30 13:57:10 -0300

    add 404 page

--- pages/_error.js
+++ pages/_error.js
@@ -1,11 +1,30 @@
+import Header from "../components/Header";
+import SingleStep from "../components/SingleStep";
+import Link from "next/link";
+
 export default function Error() {
   return (
-    <section>
-      <h1>404</h1>
-      <h3>Page not found</h3>
-      <a href="/" className="button">
-        Back to homepage
-      </a>
-    </section>
+    <main>
+      <Header first />
+      <SingleStep
+        isFirst
+        prefix="Sorry the page you're looking for was not found."
+      >
+        <h1>404</h1>
+        <h3>Page not found</h3>
+        <Link href="/">
+          <a>Back to homepage</a>
+        </Link>
+      </SingleStep>
+      <style jsx>{`
+        main {
+          display: block;
+          height: 88vh;
+          max-width: 700px;
+          margin: 0 auto;
+          text-align: center;
+        }
+      `}</style>
+    </main>
   );
 }
