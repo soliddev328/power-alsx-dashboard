505c9b9b2cf075f7bc07c5fd133f4703a81be969
smartdev328 <solid.dev3028@gmail.com>
2020-02-16 12:12:06 -0300

    wip

--- pages/onboarding/step2.js
+++ pages/onboarding/step2.js
@@ -59,11 +59,21 @@ function Step2() {
           setUtility({
             project: {
               imageUrl:
-                data.projects[0].imageUrl ||
+                data?.projects[0]?.imageUrl ||
                 "/static/images/illustrations/t&c.png",
-              name: data.projects[0].displayName || false,
-              state: data.projects[0].state || false,
-              completion: data.projects[0].completion || false
+              name: data?.projects[0]?.displayName || false,
+              state: data?.projects[0]?.state || false,
+              completion: data?.projects[0]?.completion || false
+            },
+            billingMethod: storedBillingMethod
+          });
+        } else if (data) {
+          setUtility({
+            project: {
+              imageUrl: "/static/images/illustrations/t&c.png",
+              name: data?.utility || false,
+              state: data?.state || false,
+              completion: false
             },
             billingMethod: storedBillingMethod
           });
@@ -74,12 +84,8 @@ function Step2() {
 
   useEffect(() => {
     global.analytics.page("Step 2");
-    let isLMI = "";
-
-    if (localStorage.getItem("offer")) {
-      isLMI = localStorage.getItem("offer") === "lmi";
-      console.log("isLMI:", isLMI);
-    }
+    let offer = localStorage.getItem("offer");
+    let isLMI = offer === "lmi";
 
     setLMI(isLMI);
 
@@ -171,8 +177,7 @@ function Step2() {
         <Button
           primary
           onClick={() => {
-            if (LMI && utility?.state === "MD") {
-              console.log("state is MD and LMI is true");
+            if (LMI && utility?.project?.state === "MD") {
               router.push({
                 pathname: "/onboarding/stepLMI",
                 query: {
@@ -180,22 +185,21 @@ function Step2() {
                 }
               });
             } else {
-              console.log("ah ah ah");
-              // if (utility?.billingMethod?.indexOf("paper") >= 0) {
-              //   router.push({
-              //     pathname: "/onboarding/step4",
-              //     query: {
-              //       next: true
-              //     }
-              //   });
-              // } else {
-              //   router.push({
-              //     pathname: "/onboarding/step3",
-              //     query: {
-              //       next: true
-              //     }
-              //   });
-              // }
+              if (utility?.billingMethod?.indexOf("paper") >= 0) {
+                router.push({
+                  pathname: "/onboarding/step4",
+                  query: {
+                    next: true
+                  }
+                });
+              } else {
+                router.push({
+                  pathname: "/onboarding/step3",
+                  query: {
+                    next: true
+                  }
+                });
+              }
             }
           }}
         >
@@ -239,9 +243,6 @@ function Step2() {
         .disclaimer {
           text-align: center;
           font-size: 0.8rem;
-          margin-top: 0;
-        }
-        :global(.checkbox__label) {
           margin-top: 0;
         }
         .disclaimer {
@@ -301,6 +302,9 @@ function Step2() {
           }
           .content aside:last-child {
             padding: 30px;
+          }
+          .state-graphic {
+            bottom: 0;
           }
         }
       `}</style>
--- pages/onboarding/stepLMI.js
+++ pages/onboarding/stepLMI.js
@@ -1,8 +1,11 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
+import { Formik, Form } from "formik";
 import axios from "axios";
 import Header from "../../components/Header";
 import Text from "../../components/Text";
+
+import Checkbox from "../../components/Checkbox";
 import SingleStep from "../../components/SingleStep";
 import Button from "../../components/Button";
 import CONSTANTS from "../../globals";
@@ -21,14 +24,49 @@ function StepLMI() {
     <main>
       <Header />
       <SingleStep title="Great! Let's see if you qualify for a 25% discount under MD's low and moderate income program (if you don't tou still save 10%)">
-        <Button
-          primary
-          onClick={() => {
-            console.log("state is MD and LMI is true");
+        <Formik
+          initialValues={{}}
+          onSubmit={values => {
+            // authenticate(values);
           }}
         >
-          Next
-        </Button>
+          {props => (
+            <Form>
+              {/* <ZipCodeInput
+                value={props.values.postalCode}
+                onChangeEvent={props.setFieldValue}
+                onBlurEvent={props.setFieldTouched}
+                label="ZipCode"
+                fieldname="postalCode"
+              />
+              <CustomSelect
+                ref={selectRef}
+                zipCode={props.values.postalCode}
+                value={props.currentUtility}
+                disabled={!props.values.postalCode}
+                onChange={props.setFieldValue}
+                onBlur={props.setFieldTouched}
+                touched={props.touched}
+                fieldname="currentUtility"
+              /> */}
+              <Checkbox fieldname="attestInformation">
+                <p className="checkbox__label">
+                  I attest that the above information is accurate. I understand
+                  that this information will be used to determine my eligibility
+                  as a low or moderate income household.
+                </p>
+              </Checkbox>
+              <Button
+                primary
+                onClick={() => {
+                  console.log("state is MD and LMI is true");
+                }}
+              >
+                Next
+              </Button>
+            </Form>
+          )}
+        </Formik>
       </SingleStep>
       <style jsx>{`
         main {
