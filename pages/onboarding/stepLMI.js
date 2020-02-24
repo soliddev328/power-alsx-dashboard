a15c9b4806698e1051fbf13103ca51e4ae90db06
smartdev328 <solid.dev3028@gmail.com>
2020-02-24 18:15:57 -0500

    saving household members in the db

--- pages/onboarding/stepLMI.js
+++ pages/onboarding/stepLMI.js
@@ -56,6 +56,8 @@ function StepLMI(props) {
           `${API}/v1/subscribers`,
           {
             leadId: leadId,
+            lmiProgram: true,
+            householdMembers: values.householdMembers.value,
             ssn: values.userSSN,
             spouseFirstName: values.spouseFirstName,
             spouseLastName: values.spouseLastName,
